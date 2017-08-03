d14e479d643081459539c9131d5ccda1f980b884
Ewout Kramer <e.kramer@furore.com>
2017-08-03 15:08:50 +0200

    feature: Refresh of UI & addition of TX support

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Furore.Fhir.ValidationDemo.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -44,6 +44,90 @@ namespace Furore.Fhir.ValidationDemo.Properties {
             }
             set {
                 this["InstanceXml"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string TerminologyService {
+            get {
+                return ((string)(this["TerminologyService"]));
+            }
+            set {
+                this["TerminologyService"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UseBuiltInTX {
+            get {
+                return ((bool)(this["UseBuiltInTX"]));
+            }
+            set {
+                this["UseBuiltInTX"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://ontoserver.csiro.au/stu3-latest|http://tx.fhir.org/r3")]
+        public string TerminologyServiceList {
+            get {
+                return ((string)(this["TerminologyServiceList"]));
+            }
+            set {
+                this["TerminologyServiceList"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool EnableTrace {
+            get {
+                return ((bool)(this["EnableTrace"]));
+            }
+            set {
+                this["EnableTrace"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool DoXsdValidation {
+            get {
+                return ((bool)(this["DoXsdValidation"]));
+            }
+            set {
+                this["DoXsdValidation"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool DisableFP {
+            get {
+                return ((bool)(this["DisableFP"]));
+            }
+            set {
+                this["DisableFP"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool RegenerateSnapshot {
+            get {
+                return ((bool)(this["RegenerateSnapshot"]));
+            }
+            set {
+                this["RegenerateSnapshot"] = value;
             }
         }
     }
